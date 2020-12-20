6ce8369d0c60a829fca23c3de0d4a786bde7b5a0
Matthew Krieg <66855036+mdkrieg@users.noreply.github.com>
2020-12-20 04:50:25 -0600

    Add files via upload

---
+++ j2e.js
@@ -0,0 +1,36 @@
+module.exports = function (RED) {
+    "use strict";
+    var json2xl = require('json2excel');
+
+    function ConvertJSONToExcel(config) {
+        
+        RED.nodes.createNode(this, config);
+        this.file = config.file;
+        this.on('input', function(msg){
+            var node = this;
+            var file = msg.filepath;
+            if(file === undefined || file === null){
+               if(node.file === undefined || node.file === null ){
+                   console.log("File path cannot be null");
+                   node.error("filepath cannot be null");
+               }else{
+                   file = node.file;
+               }
+            }
+            if(msg.payload === undefined || msg.payload === null){
+                console.log("Payload cannot be blank");
+                node.error("payload cannot be null");
+            }else{
+                json2xl.j2e({
+                    sheets      :msg.payload,
+                    filepath    :file
+                },function(){
+                    node.send(msg);
+                });
+            }
+        });
+        
+    };
+
+    RED.nodes.registerType("excel", ConvertJSONToExcel);
+}
