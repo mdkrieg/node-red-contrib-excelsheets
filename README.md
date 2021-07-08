d29aaefee96b28ea851d4413ea18da089b784099
Matthew Krieg <mdkrieg@mtu.edu>
2021-07-08 18:43:13 -0500

    added flexible input structure

--- README.md
+++ README.md
@@ -6,40 +6,71 @@ Features the ability to save data to multiple sheets. I found it wasn't supporte
 
 To see an example, install the package, then in your Node-RED environment go to Import > Examples > node-red-contrib-excelsheets
 
-Basic **msg** format is,
+There are **two methods** of structuring your incoming **msg**,
+
+Standard format is this:
 
 ```
 payload: [
     {
         header: {
             col1: "Column 1",
-            col2: "Column 2",
-            ...
+            col2: "Column 2"
         } ,
         items: [
             {
                 col1: "data",
-                col2: "data",
-                ...
+                col2: "data"
             }
         ] ,
         sheetName: "Sheet 1"
     } ,
     {header, items, sheetName} ,
     {header, items, sheetName} ,
-    {header, items, sheetName} ,
-    ...
+    {header, items, sheetName} 
 ] ,
 filepath: "output.xlsx"
 ```
 
+Flexible format is this:
+
+```
+payload: {
+    sheet1: [
+        header1: "data",
+        header2: "data",
+        header3:{
+            sub1: "data",
+            sub2: "data"
+        }
+    ],
+    sheet2: [
+        header1: "data",
+        header2: "data"
+    ],
+    sheet3: [
+        header1: "data",
+        header2: "data"
+    ]
+} ,
+filepath: "output.xlsx"
+```
+
+When using the "Flexible" format, the headers are automatically determined from the key names. If the data in the sheet is nested in multiple objects, the path to that data is concatenated with hyphens recursively.
+
+For the example above: sheet1, will have four headers: "header1", "header2", "header3-sub1", and "header3-sub2". sheet2 and sheet3 will just have "header1" and "header2". Furthermore, the headers do **not** need to be consistent, even in the same sheet. Unique headers are added to the header list when they are encountered.
+
+The output of the node will always transform the data into the "Standard" format.
+
 ## Change Log
+* 1.1.0 - Created "Flexible" input option that parses object keys as sheet names and recursively parses multi-level objects in the row data
 * 1.0.5 - Updated docs to refer to correct msg property: filepath (not filename, )
 * 1.0.4 - Additional Example "basic w server". Cleanup and documentation.
 * 1.0.1 ~ 3 - Cleanup and documentation.
 * 1.0.0 - Original version, forked from "node-red-contrib-excel" and swapped out a different dependency.
 
 ## TODO
-- [ ] Give richer status feedback / error feedback
-- [ ] Digging deeper into the dependencies, there's a really powerful package called "xlsx" [<npmjs.com>](https://www.npmjs.com/package/xlsx) that has a rich base of features, I would like to create a node that uses everything available.
-- [ ] Come up with a few examples that do something more interesting
+- [ ] Confirm where files are saved if no path is given
+- RE: above, make it default to $HOME dir if it doesn't already
+
+Digging deeper into the dependencies, there's a really powerful package called "xlsx" [<npmjs.com>](https://www.npmjs.com/package/xlsx) that has a very rich base of features. It would be nice to create a node that uses everything available, maybe someday.
--- xl.js
+++ xl.js
@@ -2,38 +2,87 @@ module.exports = function (RED) {
     "use strict";
     var json2xl = require('json2excel');
 
+    function isObject(input_obj){
+        return (typeof input_obj === "object" &&  input_obj != null);
+    }
+    
     function ConvertJSONToExcel(config) {
-        
         RED.nodes.createNode(this, config);
-        this.file = config.file;
         this.on('input', function(msg){
             var node = this;
-            var file = msg.filepath;
-            if(file === undefined || file === null){
-               if(node.file === undefined || node.file === null ){
-                   console.log("File path cannot be null");
-                   node.error("filepath cannot be null");
-               }else{
-                   file = node.file;
-               }
+            var file = msg.filepath || config.file;
+            if(file === undefined || file === null ){
+                node.error("filepath cannot be null");
+                return;
             }
             if(msg.payload === undefined || msg.payload === null){
-                console.log("Payload cannot be blank");
                 node.error("payload cannot be null");
-            }else if(!Array.isArray(msg.payload)){
-                console.log("Payload must be Array");
-                node.error("payload must be Array");
-            }else{
-                json2xl.j2e({
-                    sheets      :msg.payload,
-                    filepath    :file
-                },function(){
-                    node.send(msg);
-                });
+                return;
             }
+            if(isObject(msg.payload)){
+                msg.payload = convertObjectToArray(msg.payload);
+            }
+            json2xl.j2e({
+                sheets      :msg.payload,
+                filepath    :file
+            },function(){
+                node.send(msg);
+            });
         });
+    }
+    function convertObjectToArray(obj){
+        let output = [];
         
-    };
-
+        // LEVEL 3+ - disseminate arbitrary objects
+        function handleRowObject(row_obj, prefix = []){
+            let output_row = {};
+            // handles rows as well as "layered" objects inside the rows
+            //   the prefix variable is used to carry through the path
+            for(var key in row_obj){
+                let data = handleDataItem(row_obj, key, prefix);
+                // merge the handled data to our output row
+                if(Array.isArray(data)){
+                    // returns array if primitive item
+                    // NOTE: every branch eventually gets here
+                    output_row[data[0]] = data[1];
+                }else if(isObject(data)){
+                    // returns object if complex item
+                    output_row = {...output_row, ...data};
+                }
+            }
+            return output_row;
+        }
+        function handleDataItem(parent_obj, data_key, prefix){
+            // looks if it's an object then it recurses through
+            let content = parent_obj[data_key];
+            let full_prefix = [...prefix, ...[data_key]];
+            if(isObject(content)){
+                return handleRowObject(content, full_prefix);
+            }
+            return [full_prefix.join("-"), content];
+        }
+        
+        // LEVEL 1 - object keys are sheet names
+        for(var sheet in obj){
+            let sheet_obj = {
+                header: {},
+                items: [],
+                sheetName: sheet
+            };
+            // LEVEL 2 - keys contain arrays of data
+            if(!Array.isArray(obj[sheet])) continue;
+            for(var row of obj[sheet]){
+                var flat_row = handleRowObject(row);
+                for(var key_name in flat_row){
+                    // headers are just simply the key names
+                    // this method is conveniently passive on duplicates
+                    sheet_obj.header[key_name] = key_name;
+                }
+                sheet_obj.items.push(flat_row);
+            }
+            output.push(sheet_obj);
+        }
+        return output;
+    }
     RED.nodes.registerType("excelsheets", ConvertJSONToExcel);
 }
